584a63c39836a4e246ab445b2a704eaeedd39338
dphuang2 <dylan.p.huang@gmail.com>
2023-08-29 19:17:55 -0700

    add test for multiple requests in Python SDK

--- sdks/python/test/test_simple.py
+++ sdks/python/test/test_simple.py
@@ -15,6 +15,7 @@ import os
 from pprint import pprint
 from leap import Leap
 
+MOCK_SERVER_URL = "http://localhost:4010"
 class TestSimple(unittest.TestCase):
     def setUp(self):
         pass
@@ -24,11 +25,20 @@ class TestSimple(unittest.TestCase):
             # Defining the host is optional and defaults to https://api.tryleap.ai
             # See configuration.py for a list of all supported configuration parameters.
             host = "https://api.tryleap.ai",
-        
+
             # Configure Bearer authorization (JWT): bearer
             access_token = 'YOUR_BEARER_TOKEN'
         )
         self.assertIsNotNone(leap)
+
+    def test_multiple_requests(self):
+        leap = Leap(
+            host=MOCK_SERVER_URL,
+            access_token="TEST"
+        )
+        inference = leap.generate_images.create(model_id="test", prompt="A red bird")
+        images = leap.generate_images.find_one(model_id="test", inference_id=inference.body["id"])
+        self.assertIsNotNone(images.body)
 
     def tearDown(self):
         pass
