30d21671112c68a78de4b3aa89019d65646ad941
konfig-publisher <publisher@konfigthis.com>
2023-08-31 21:09:56 -0700

    patch bump + regenerate SDKs

--- sdks/java/src/main/java/com/konfigthis/leap/client/ApiClient.java
+++ sdks/java/src/main/java/com/konfigthis/leap/client/ApiClient.java
@@ -143,7 +143,7 @@ public class ApiClient extends ApiClientCustom {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("Konfig/1.1.1/java");
+        setUserAgent("Konfig/1.1.2/java");
 
         authentications = new HashMap<String, Authentication>();
     }
